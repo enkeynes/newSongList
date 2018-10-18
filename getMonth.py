1c17efa2bb3dbd375a61cd285ce3366474d18e2b
enkeynes <harvey.road51216@gmail.com>
2018-10-18 22:11:39 +0900

    Create getMonth.py

---
+++ getMonth.py
@@ -0,0 +1,29 @@
+#coding:utf-8
+
+def getMonth(m):
+    if m == "Jun":
+        return 1
+    elif m == "Feb":
+        return 2
+    elif m == "Mar":
+        return 3
+    elif m == "Apr":
+        return 4
+    elif m == "May":
+        return 5
+    elif m == "Jun":
+        return 6
+    elif m == "Jul":
+        return 7
+    elif m == "Aug":
+        return 8
+    elif m == "Sep":
+        return 9
+    elif m == "Oct":
+        return 10
+    elif m == "Nov":
+        return 11
+    elif m == "Dec":
+        return 12
+    else:
+        return None
